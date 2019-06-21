b7b16795f37cfb61df6efcfdf1e9c620baa41e73
homerooliveira <homero.santos.001@acad.pucrs.br>
2019-06-20 22:02:22 -0300

    testing

--- sequencial.c
+++ sequencial.c
@@ -90,7 +90,7 @@ void copyMatrix(int start, int lines, int m1[SIZE][SIZE], int matrix[lines][SIZE
     int i, j;
     for(i = start; i < lines; i++) {
         for(j = 0; j < SIZE; j++) {
-            matrix[i][j] = m1[i][j];
+            m1[i][j] = matrix[i][j];
         }
     }
 }
