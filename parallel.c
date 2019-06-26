ced33f3280ffe66d10880b1832481b3c0f8531ba
Rafael Cadaval <rafaelcadaval@gmail.com>
2019-06-26 09:00:29 -0300

    Debug messaging logic

--- parallel.c
+++ parallel.c
@@ -373,7 +373,9 @@ int main(int argc, char** argv) {
         if(status.MPI_TAG == NUMBER_OF_ROWS_TAG) {
             int num_rows;
             MPI_Recv(&num_rows, buffer_count, MPI_INT, MASTER_RANK, NUMBER_OF_ROWS_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            printf("** num_rows %d received**\n", num_rows);
             MPI_Probe(MASTER_RANK, SENDING_LINES_TO_PROCESS_TAG, MPI_COMM_WORLD, &status);
+            printf("** hello? **\n");
             MPI_Get_count(&status, MPI_INT, &buffer_count);
             int recv_matrix[num_rows][SIZE];
             MPI_Recv(&recv_matrix, buffer_count, MPI_INT, MASTER_RANK, SENDING_LINES_TO_PROCESS_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
