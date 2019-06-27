71b44382f3dc05b5b5bdbb69f621d6dcc0cb707b
15110322 <15110322@facin.portoalegre.pucrsnet.br>
2019-06-27 16:30:33 -0300

    Debug pending processes

--- parallel.c
+++ parallel.c
@@ -325,10 +325,14 @@ int main(int argc, char** argv) {
         printf("\n\n*************************************\n");
         printf("Execution total time: %f seconds\n", execution_elapsed_time);
         printf("*************************************\n\n");
+
+        MPI_Finalize();
     } else {
         int has_rows_left = 1;
         while(has_rows_left) {
-            // printf("** Worker %d requesting lines **\n", my_rank);
+            printf("** Worker %d requesting lines **\n", my_rank);
+            printf("Rank: %d | Hostname: %s | Rows left: %d\n", my_rank, hostname, has_rows_left);
+
             // printf("nmber of lines %d\n", MAX_NUMBER_OF_LINES);
             MPI_Send(&MAX_NUMBER_OF_LINES, 1, MPI_INT, MASTER_RANK, NEED_LINES_TO_PROCESS_TAG, MPI_COMM_WORLD);
 
@@ -353,9 +357,9 @@ int main(int argc, char** argv) {
             MPI_Recv(&has_rows_left, 1, MPI_INT, MASTER_RANK, HAS_ROWS_LEFT_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
             printf("Rank: %d | Hostname: %s | Rows left: %d\n", my_rank, hostname, has_rows_left);
         }
+        MPI_Finalize();
     }
 
     printf("Rank: %d morreu\n", my_rank);
-    MPI_Finalize();
     return 0;
 }
