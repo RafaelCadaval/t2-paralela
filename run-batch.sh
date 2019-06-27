690200e79783e9caf5dcb4db66cf138fd4f6d8a4
Homero Oliveira <homero.oliveira@poatek.com>
2019-06-27 18:38:05 -0300

    fix

--- run-batch.sh
+++ run-batch.sh
@@ -4,7 +4,9 @@ i=100
 while [ "$i" -le 2000 ]; do
     j=1
     while [ "$j" -le 10 ]; do
-        echo "$i - $j " && qsub batchjob-2/Batchjob-2-proc >> output.txt 
+        mpicc parallel.c -o parallel -DSIZE=2000 -std=c99 -fopenmp &&
+        echo "$i - $j " && 
+        qsub batchjob-2/Batchjob-2-proc >> output.txt 
         j=$(( j + 1 ))
     done
     i=$(( i + 100 ))
